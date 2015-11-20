12f79ba15dbb824164030200f1e1d826c4b8a161
Christopher MANEU <christopher@maneu.net>
2015-11-20 20:16:25 +0100

    Commit first version of the project

---
+++ loghash-testgen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace loghash_testgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestGen generator = new TestGen();
+                generator.GenerateFromFile(args[0], args[1]);
+            }
+            catch (Exception ex)
+            {
+
+                Console.Write(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ loghash-testgen/TestGen.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace loghash_testgen
+{
+    class TestGen
+    {
+        private const string TITLE_TAG = "#>";
+
+        public void GenerateFromFile(string filePath, string outputDirectory)
+        {
+            if (string.IsNullOrWhiteSpace(outputDirectory))
+                throw new ArgumentNullException(nameof(outputDirectory));
+
+            if (!File.Exists(filePath))
+            {
+                throw new ArgumentException("File does not exist.", nameof(outputDirectory));
+                return;
+            }
+
+            var lines = File.ReadLines(filePath);
+
+            string currentLine;
+            IEnumerator<string> enumerator = lines.GetEnumerator();
+            enumerator.MoveNext();
+
+            TestCase currentTestCase = null;
+            do
+            {
+                if (enumerator.Current.StartsWith(TITLE_TAG, StringComparison.CurrentCulture))
+                {
+                    if (currentTestCase != null)
+                    {
+                        WriteTestCase(outputDirectory, currentTestCase);
+                        currentTestCase = null;
+                    }
+
+                    currentTestCase = new TestCase();
+                    currentTestCase.Name = enumerator.Current.Substring(TITLE_TAG.Length);
+                    enumerator.MoveNext();
+                    currentTestCase.Content = enumerator.Current;
+                    continue;
+                }
+
+                currentTestCase.Json += enumerator.Current + Environment.NewLine;
+
+            } while (enumerator.MoveNext());
+
+
+
+            if (currentTestCase != null)
+            {
+                WriteTestCase(outputDirectory, currentTestCase);
+                currentTestCase = null;
+            }
+        }
+
+        private void WriteTestCase(string outputDirectory, TestCase currentTestCase)
+        {
+            EnsureDirectoryExists(outputDirectory);
+
+            File.WriteAllText(Path.Combine(outputDirectory, currentTestCase.Name + ".txt"), currentTestCase.Content);
+            File.WriteAllText(Path.Combine(outputDirectory, currentTestCase.Name + ".json"), currentTestCase.Json);
+        }
+
+        private void EnsureDirectoryExists(string outputDirectory)
+        {
+            if (Directory.Exists(outputDirectory))
+                return;
+
+            Directory.CreateDirectory(outputDirectory);
+        }
+
+        class TestCase
+        {
+            public string Name { get; set; }
+            public string Content { get; set; }
+            public string Json { get; set; }
+        }
+    }
+}
